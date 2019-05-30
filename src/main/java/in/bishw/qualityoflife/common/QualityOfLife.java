8449812a3a794b832bc9840c54d091dd6690c5d0
Jake <jake.huggins@solirius.com>
2019-05-30 19:56:15 +0100

    add QualityOfLife mod init class

---
+++ src/main/java/in/bishw/qualityoflife/common/QualityOfLife.java
@@ -0,0 +1,30 @@
+package in.bishw.qualityoflife.common;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = QualityOfLife.MODID,
+        name = QualityOfLife.NAME,
+        version = QualityOfLife.VERSION)
+public class QualityOfLife {
+    public static final String MODID = "qol";
+    public static final String NAME = "Quality of Life";
+    public static final String VERSION = "0.0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
