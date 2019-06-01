6a005b68da2b540570c29fb0ea6061a642850ef6
Jake <jake.huggins@solirius.com>
2019-06-01 17:10:19 +0100

    update fast_furnace to use multitextures
    updating fast_furnace blockstate json to use new textures
    updating fast_furnace class to use blockstate to save block direction

--- src/main/java/in/bishw/qualityoflife/common/furnace/BlockFastFurnace.java
+++ src/main/java/in/bishw/qualityoflife/common/furnace/BlockFastFurnace.java
@@ -3,24 +3,36 @@ package in.bishw.qualityoflife.common.furnace;
 import in.bishw.qualityoflife.common.QualityOfLife;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+
 public class BlockFastFurnace extends Block {
 
+
+    public static final PropertyDirection FACING = PropertyDirection.create("facing");
     public static final ResourceLocation FAST_FURNACE = new ResourceLocation(QualityOfLife.MODID, "fast_furnace");
 
-    public BlockFastFurnace(){
+    public BlockFastFurnace() {
         super(Material.IRON);
         setRegistryName(FAST_FURNACE);
         setUnlocalizedName(QualityOfLife.MODID + ".fast_furnace");
         setHardness(1.5F);
         setHarvestLevel("axe", 2);
         setCreativeTab(QualityOfLife.creativeTab);
+
+        setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
     }
 
     @SideOnly(Side.CLIENT)
@@ -28,7 +40,27 @@ public class BlockFastFurnace extends Block {
         ModelLoader.setCustomModelResourceLocation(
                 Item.getItemFromBlock(this),
                 0,
-                new ModelResourceLocation(getRegistryName(),"inventory"));
+                new ModelResourceLocation(getRegistryName(), "inventory"));
     }
 
+    @Override
+    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return this.getDefaultState()
+                .withProperty(FACING, EnumFacing.getDirectionFromEntityLiving(pos, placer));
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(FACING, EnumFacing.getFront(meta & 7));
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, FACING);
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).getIndex();
+    }
 }
